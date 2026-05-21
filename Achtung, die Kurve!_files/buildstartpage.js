63b2128f94b95349d2a20458259a641428de28c5
pekksis <pekkala.ville@gmail.com>
2026-05-21 18:15:44 +0300

    Add files via upload

---
+++ Achtung, die Kurve!_files/buildstartpage.js
@@ -0,0 +1,8 @@
+const gameID = document.querySelector("#game")
+const startPage = document.querySelector("#start_page")
+function buildStartPage() {
+    
+    playButton.addEventListener("click", pressSpace)
+}
+
+buildStartPage()
