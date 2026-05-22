edafd322472bbcffe0cb29b0738cbfd2fbf43060
pekksis <pekkala.ville@gmail.com>
2026-05-22 21:32:54 +0300

    volumes

--- Achtung, die Kurve!_files/script.js
+++ Achtung, die Kurve!_files/script.js
@@ -100,7 +100,10 @@ const fredDeathClips = [
     new Audio("Achtung, die Kurve!_files/death2.mp3"),
 	new Audio("Achtung, die Kurve!_files/death3.mp3")
 ]
-fredDeathClips[1].volume = 0.4 //lower this volume
+fredDeathClips[1].volume = 0.3 //lower this volume
+fredDeathClips[0].volume = 0.8 //lower this volume
+fredDeathClips[2].volume = 0.8 //lower this volume
+
 const ghostModeAudio = new Audio("Achtung, die Kurve!_files/tico.mp3")
 ghostModeAudio.loop = true
 
